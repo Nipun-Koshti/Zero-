cb9d1fc7184310866fef811184ba556049941fe6
Nipun <koshtinipun2002@gmail.com>
2025-09-08 01:04:12 +0530

    model design

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on('error',(err)=>{
+        console.error("Error in DB connection",err);
+        throw err;
+       })
+    app.listen(process.env.PORT||8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT||8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed !!!!", err);
+});
 
 
 
